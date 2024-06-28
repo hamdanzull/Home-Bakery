268007790b692a643065eb423bed35b152517ac6
hamdanzull <hmdan.zulfa@gmail.com>
2024-06-28 09:29:47 +0700

    add react GA

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import HomePage from "./pages/HomePage"
+import ReactGA from "react-ga4";
+
+const trackingId = "G-RFM1BS2D12"
+ReactGA.initialize(trackingId);
 
 function App() {
+  useEffect(() => {
+    ReactGA.send({ hitType: "pageview", page: "/" });
+  }, [])
 
   return (
     <Router>
