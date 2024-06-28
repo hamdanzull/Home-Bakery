5e0391ee8e79e19b3cec2c0c84eb3ee618671c01
hamdanzull <hmdan.zulfa@gmail.com>
2024-06-28 22:51:58 +0700

    add hero section

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import Header from '../components/Header/Header';
-import Hero from '../components/Hero/Hero';
+import Hero from '../components/Hero/hero';
 
 export default function HomePage() {
     return (
